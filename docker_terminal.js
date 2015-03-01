eca61a744d542e197e2fdf74e4d1f673c5feee38
Aidan Hobson Sayers <aidanhs@cantab.net>
2015-03-01 21:11:57 +0000

    Remaining event handling to react

--- docker_terminal.js
+++ docker_terminal.js
@@ -41,39 +41,8 @@ window.docker = (function() {
     }
   };
 
-
   return docker;
 })();
-
-function containername (container) {
-  if (container.Names != undefined && container.Names.length > 0) {
-    return container.Names[0].replace('/','') + ' (' + container.Id.slice(0,6) + ')';
-  } else if (container.Name != undefined) {
-    // This was obtained with a call to /containers/<ID>/json, note ID is different to Id
-    return container.Name.replace('/','') + ' (' + container.ID.slice(0,6) + ')';
-  } else {
-    return container.Id.slice(0,6);
-  }
-}
-window.initHandlers = function () {
-  $('#refresh_containers').on('click', function () {
-    $('#setting_container').empty();
-    $.get(ApiUrl() + '/v1.6/containers/json', function (d) {
-      d.map(function (container) {
-        var e = $('<option>').text(containername(container) + ' - ' + container.Image).val(container.Id);
-        $(e).data(container);
-        $('#setting_container').append(e);
-      });
-    });
-  });
-
-  $('#attach_container').on('click', function () {
-    $('.terminal').remove();
-    var container = $('#setting_container option:selected').data();
-    docker.terminal.startTerminalForContainer($('#setting_host').val(), container.Id);
-    $('#container_id').text(containername(container));
-  });
-};
 
 //$(function() {
 //  $("[data-docker-terminal-container]").each(function(i, el) {
